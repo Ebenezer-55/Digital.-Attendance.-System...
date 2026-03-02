e05cca08e49eeb205dc80a0460f3931deb92caa3
Ebenezer-55 <tettehebenezer139@gmail.com>
2026-03-02 07:57:14 -0800

    Week 4: File saving added Final Submission

--- digital attendance system@/main.cpp
+++ digital attendance system@/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 using namespace std;
+#include <fstring>
 class AttendanceSession {
 public:
     string courseCode;
@@ -134,6 +135,27 @@ void markAttendance() {
 }
 
 void attendanceSummary() {
+    void saveAttendanceToFile() {
+
+    ofstream file("attendance.txt");
+
+    if(!file) {
+        cout << "Error saving file!" << endl;
+        return;
+    }
+
+    file << "Attendance Record\n";
+
+    for(int i=0; i<studentCount; i++) {
+        file << students[i].id << " "
+             << students[i].name << " "
+             << attendanceStatus[i] << endl;
+    }
+
+    file.close();
+
+    cout << "Attendance saved to file successfully!\n";
+}
     int present=0, absent=0, late=0;
 
     for(int i=0; i<studentCount; i++) {
@@ -180,4 +202,5 @@ int main() {
     return 0;
 
 }
+
 
